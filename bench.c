7611e84504a89bd35c2d00fed05108a3b627b811
Hussain Al Marzooq <marzooqy@users.noreply.github.com>
2025-10-10 15:18:29 +0300

    Initial commit

---
+++ bench.c
@@ -0,0 +1,80 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "crc.h"
+
+uint64_t ten_pow(int8_t n) {
+    uint64_t i = 1;
+
+    while(n-- > 0) {
+        i *= 10;
+    }
+
+    return i;
+}
+
+void bench(uint64_t n, uint64_t len) {
+    if(len < ten_pow(3)) {
+        printf("| %lli B |", len);
+    } else if(len < ten_pow(6)) {
+        printf("| %lli KB |", len / ten_pow(3));
+    } else {
+        printf("| %lli MB |", len / ten_pow(6));
+    }
+
+    clock_t start, end;
+    float speed;
+    params_t params;
+
+    unsigned char* buf = (unsigned char*) malloc(len);
+
+    for(uint64_t i = 0; i < len; i++) {
+        buf[i] = i & 0xff;
+    }
+
+    //Reflected
+    params = crc_params(64, 0x42f0e1eba9ea3693, 0xffffffffffffffff, true, true, 0xffffffffffffffff);
+
+    start = clock();
+
+    for(uint32_t i = 0; i < n; i++) {
+        crc_clmul(&params, params.init, buf, len);
+    }
+
+    end = clock();
+
+    speed = (float)(len * n) / ((float)(end - start) / CLOCKS_PER_SEC) / (1024.0 * 1024.0);
+
+    printf(" %.2f MiB/s ", speed);
+
+    //Non-Reflected
+    params = crc_params(64, 0x42f0e1eba9ea3693, 0xffffffffffffffff, false, false, 0xffffffffffffffff);
+
+    start = clock();
+
+    for(uint32_t i = 0; i < n; i++) {
+        crc_clmul(&params, params.init, buf, len);
+    }
+
+    end = clock();
+
+    speed = (float)(len * n) / ((float)(end - start) / CLOCKS_PER_SEC) / (1024.0 * 1024.0);
+
+    printf("| %.2f MiB/s |\n", speed);
+
+    free(buf);
+}
+
+void main() {
+    printf("| Length | Reflected | Non-Reflected |\n");
+    printf("| --- | --- | --- |\n");
+
+    bench(ten_pow(7), ten_pow(2));
+    bench(ten_pow(7), ten_pow(3));
+    bench(ten_pow(7), ten_pow(4));
+    bench(ten_pow(6), ten_pow(5));
+    bench(ten_pow(5), ten_pow(6));
+    bench(ten_pow(4), ten_pow(7));
+    bench(ten_pow(3), ten_pow(8));
+}
---
+++ crc.h
@@ -0,0 +1,65 @@
+#ifndef CRC_CLMUL_H
+#define CRC_CLMUL_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Holds frequently used CRC parameters.
+   Most of these are due to translating the algorithm from hardware to software.
+
+   width is the width of the polynomial.
+
+   The polynomial is the divisor in the CRC algorithm.
+
+   spoly is the "scaled" polynomial, which is multiplied by x^(64 - w). In the non-reflected table algorithm,
+   this truncates any bits that are shifted out of the register from the left. In the non-reflected SIMD algorithm,
+   this converts any CRC to 64 bits (Intel paper p16), allowing us to use the same algorithm for all CRC parameters
+   without any adjustments. This has the additional benefit of using the same alignment for both algorithms.
+
+   rpoly is the reflected polynomial, which is used when refin is true.
+
+   Polynomials have an implicit x^(w+1) factor, which is typically not included in code.
+   The CRC could be computed without it, and CRC64 would require a larger integer if it were used.
+
+   refin specifies if the incoming bytes should be reflected before being used to compute the CRC.
+   Alternatively we could "reflect the world" (Ross Williams guide section 11).
+   That is, we reflect the init, poly, and reverse the algorithm.
+
+   refout specifies if result should be reflected at the end of the calculation.
+
+   init is the initial content of the register. It's XORed with the first few incoming bits.
+   pycrc labels it more clearly as xor_in.
+
+   xorout is XORed with the CRC at the end of the calculation.
+
+   k1 and k2 are the constant used to fold the buffer (Intel paper p12).
+
+   table holds the values for the byte-by-byte (or Sarwate) algorithm.
+   It's the result of computing the CRC for every possible input byte.*/
+
+typedef struct {
+    uint8_t width;
+    uint64_t spoly;
+    uint64_t rpoly;
+    bool refin;
+    bool refout;
+    uint64_t init;
+    uint64_t xorout;
+    uint64_t k1;
+    uint64_t k2;
+    uint64_t table[256];
+} params_t;
+
+/* Create a params_t struct and initialize it with the provided parameters.
+   Calculate the values of k1, k2, and the table. */
+params_t crc_params(uint8_t width, uint64_t poly, uint64_t init, bool refin, bool refout, uint64_t xorout);
+
+/* Calculate a CRC using the table-based algorithm.
+   Use params.init as the initial CRC value.*/
+uint64_t crc_table(params_t *params, uint64_t crc, unsigned char const *buf, uint64_t len);
+
+/* Calculate CRC using the SIMD algorithm.
+   Use params.init as the initial CRC value.*/
+uint64_t crc_clmul(params_t *params, uint64_t crc, unsigned char const *buf, uint64_t len);
+
+#endif
