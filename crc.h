a4e22c4bd1f7b5779c17b3906ebc32b3fd596d11
Hussain Al Marzooq <marzooqy@users.noreply.github.com>
2025-10-22 00:42:59 +0300

    Add software fallback
    Also drop support for MSVC.

--- crc.h
+++ crc.h
@@ -4,12 +4,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#ifdef _MSC_VER
-    #define DllExport __declspec(dllexport)
-#else
-    #define DllExport
-#endif
-
 /* Holds frequently used CRC parameters. */
 typedef struct {
     uint8_t width;
@@ -25,14 +19,14 @@ typedef struct {
 
 /* Create a params_t struct and initialize it with the provided parameters.
    Calculate the values of k1, k2, and the table. */
-params_t DllExport crc_params(uint8_t width, uint64_t poly, uint64_t init, bool refin, bool refout, uint64_t xorout);
+params_t crc_params(uint8_t width, uint64_t poly, uint64_t init, bool refin, bool refout, uint64_t xorout);
 
 /* Calculate a CRC using the table-based algorithm.
    Use params.init as the initial CRC value.*/
-uint64_t DllExport crc_table(params_t *params, uint64_t crc, unsigned char const *buf, uint64_t len);
+uint64_t crc_table(params_t *params, uint64_t crc, unsigned char const *buf, uint64_t len);
 
 /* Calculate CRC using the SIMD algorithm. Uses the table-based algorithm if
    SIMD intrinsics are not available. Use params.init as the initial CRC value.*/
-uint64_t DllExport crc_calc(params_t *params, uint64_t crc, unsigned char const *buf, uint64_t len);
+uint64_t crc_calc(params_t *params, uint64_t crc, unsigned char const *buf, uint64_t len);
 
 #endif
