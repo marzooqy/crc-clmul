a8a5392f2453af34dddab21c4050f36223302b84
Hussain Al Marzooq <marzooqy@users.noreply.github.com>
2025-10-21 04:41:50 +0300

    Add MSVC test to CI

--- intrinsics.h
+++ intrinsics.h
@@ -7,72 +7,72 @@ static const unsigned char SWAP_TABLE[] = {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5
 
 #if defined(__x86_64__) || defined(_M_AMD64)
 
-    /* Just to clear my confusion around the selection/control bits of the PCLMULQDQ
-       instruction. 1 picks out the 64 MSBs and 0 picks the least 64. The high control
-       bit is for b and the other is for a. */
+/* Just to clear my confusion around the selection/control bits of the PCLMULQDQ
+   instruction. 1 picks out the 64 MSBs and 0 picks the least 64. The high control
+   bit is for b and the other is for a. */
 
-    // #define CLMUL(a, b, ac, bc) _mm_clmulepi64_si128(a, b, (bc ? 0x10 : 0x00) | (ac ? 0x01 : 0x00))
+// #define CLMUL(a, b, ac, bc) _mm_clmulepi64_si128(a, b, (bc ? 0x10 : 0x00) | (ac ? 0x01 : 0x00))
 
-    #include <emmintrin.h> //SSE2
-    #include <tmmintrin.h> //SSSE3
-    #include <wmmintrin.h> //AES + PCLMUL
+#include <emmintrin.h> //SSE2
+#include <tmmintrin.h> //SSSE3
+#include <wmmintrin.h> //AES + PCLMUL
 
-    typedef __m128i uint128_t;
+typedef __m128i uint128_t;
 
-    //Redundant def so the same code would work on both architectures.
-    typedef __m128i table_t;
+//Redundant def so the same code would work on both architectures.
+typedef __m128i table_t;
 
-    //Create a 128-bit integer from two 64-bit integers.
-    #define SET(hi, lo) _mm_set_epi64x(hi, lo)
+//Create a 128-bit integer from two 64-bit integers.
+#define SET(hi, lo) _mm_set_epi64x(hi, lo)
 
-    //Table for _mm_shuffle_epi8 that swaps the endianess of a 128-bit integer.
-    #define GET_SWAP_TABLE() _mm_loadu_si128((__m128i*)SWAP_TABLE)
+//Table for _mm_shuffle_epi8 that swaps the endianess of a 128-bit integer.
+#define GET_SWAP_TABLE() _mm_loadu_si128((__m128i*)SWAP_TABLE)
 
-    //Load 16 bytes from ptr into a 128-bit integer.
-    #define LOAD(ptr) _mm_loadu_si128((__m128i*)(ptr))
+//Load 16 bytes from ptr into a 128-bit integer.
+#define LOAD(ptr) _mm_loadu_si128((__m128i*)(ptr))
 
-    //Multiply the high 64-bits of two 128-bit integers.
-    #define CLMUL_HI(a, b) _mm_clmulepi64_si128(a, b, 0x11)
+//Multiply the high 64-bits of two 128-bit integers.
+#define CLMUL_HI(a, b) _mm_clmulepi64_si128(a, b, 0x11)
 
-    //Multiply the low 64-bits of two 128-bit integers.
-    #define CLMUL_LO(a, b) _mm_clmulepi64_si128(a, b, 0x00)
+//Multiply the low 64-bits of two 128-bit integers.
+#define CLMUL_LO(a, b) _mm_clmulepi64_si128(a, b, 0x00)
 
-    //Swap the endianess of a 128-bit integer.
-    #define SWAP(x, tbl) _mm_shuffle_epi8(x, tbl)
+//Swap the endianess of a 128-bit integer.
+#define SWAP(x, tbl) _mm_shuffle_epi8(x, tbl)
 
-    //XOR two 128-bit integers.
-    #define XOR(a, b) _mm_xor_si128(a, b)
+//XOR two 128-bit integers.
+#define XOR(a, b) _mm_xor_si128(a, b)
 
 #elif defined(__aarch64__) || defined(_M_ARM64)
 
-    #include <arm_neon.h>
+#include <arm_neon.h>
 
-    typedef uint64x2_t uint128_t;
-    typedef uint8x16_t table_t;
+typedef uint64x2_t uint128_t;
+typedef uint8x16_t table_t;
 
-    //Create a 64x2 vector from two 64-bit integers.
-    #define SET(hi, lo) vsetq_lane_u64(hi, vsetq_lane_u64(lo, vdupq_n_u64(0), 0), 1)
+//Create a 64x2 vector from two 64-bit integers.
+#define SET(hi, lo) vsetq_lane_u64(hi, vsetq_lane_u64(lo, vdupq_n_u64(0), 0), 1)
 
-    //Table for vqtbl1q_u8 that swaps the endianess of a 64x2 vector.
-    #define GET_SWAP_TABLE() vld1q_u8(SWAP_TABLE)
+//Table for vqtbl1q_u8 that swaps the endianess of a 64x2 vector.
+#define GET_SWAP_TABLE() vld1q_u8(SWAP_TABLE)
 
-    //Load 16 bytes from ptr into a 64x2 vector.
-    #define LOAD(ptr) vld1q_u64((const uint64_t*)(ptr))
+//Load 16 bytes from ptr into a 64x2 vector.
+#define LOAD(ptr) vld1q_u64((const uint64_t*)(ptr))
 
-    //Multiply the high lane of two 64x2 vectors.
-    #define CLMUL_HI(a, b) vreinterpretq_u64_p128(vmull_high_p64(vreinterpretq_p64_u64(a), \
-                                                                 vreinterpretq_p64_u64(b)))
+//Multiply the high lane of two 64x2 vectors.
+#define CLMUL_HI(a, b) vreinterpretq_u64_p128(vmull_high_p64(vreinterpretq_p64_u64(a), \
+                                                             vreinterpretq_p64_u64(b)))
 
-    //Multiply the low lane of two 64x2 vectors.
-    //It gets ugly.
-    #define CLMUL_LO(a, b) vreinterpretq_u64_p128(vmull_p64(vgetq_lane_p64(vreinterpretq_p64_u64(a), 0), \
-                                                            vgetq_lane_p64(vreinterpretq_p64_u64(b), 0)))
+//Multiply the low lane of two 64x2 vectors.
+//It gets ugly.
+#define CLMUL_LO(a, b) vreinterpretq_u64_p128(vmull_p64(vgetq_lane_p64(vreinterpretq_p64_u64(a), 0), \
+                                                        vgetq_lane_p64(vreinterpretq_p64_u64(b), 0)))
 
-    //Swap the endianess of a 64x2 vector.
-    #define SWAP(x, tbl) vreinterpretq_u64_u8(vqtbl1q_u8(vreinterpretq_u8_u64(x), tbl))
+//Swap the endianess of a 64x2 vector.
+#define SWAP(x, tbl) vreinterpretq_u64_u8(vqtbl1q_u8(vreinterpretq_u8_u64(x), tbl))
 
-    //XOR two 64x2 vectors.
-    #define XOR(a, b) veorq_u64(a, b)
+//XOR two 64x2 vectors.
+#define XOR(a, b) veorq_u64(a, b)
 
 #endif
 
